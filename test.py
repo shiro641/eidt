b23e6b8d21988f20f82675156a30653a779ad883
shiro641 <70186086+shiro641@users.noreply.github.com>
2021-10-01 21:18:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import sys
 '''a = int(sys.argv[1])
 b = int(sys.argv[2])'''
 file = open(sys.argv[1])
-str = file.read()
-print(str)
+str = file.read().split()
+print(str[0] + str[1])
 #print(sys.argv)
